8dba924c32a0ae627d85aa4208497ec6a088a823
TenzinSamdrup <tenzinsamdrup6@gmail.com>
2024-10-26 09:44:39 +0600

    employee list

--- resources/views/livewire/attendance-history.blade.php
+++ resources/views/livewire/attendance-history.blade.php
@@ -1,98 +1,55 @@
-<div wire:poll.1s>
-    <h2 class="mb-3" style="color:#003366">Employee List</h2>
-
-    <div class="row mb-3">
-        <div class="col-md-12">
-            <input 
-                type="text" 
-                class="form-control" 
-                placeholder="Search by name or email..." 
-                wire:model.debounce.300ms="search"
-            >
+<div>
+<div class="pos-f-t">
+        <div class="collapse" id="navbarToggleExternalContent">
+            <div class="p-4 pe-5 ps-5" style="background-color:#003366">
+                <ul class="list-unstyled d-flex">
+                    <li><a href="{{ route('employee.dashboard') }}" class="text-white ps-3 mt-3 pe-3 ms-3"><div class="ps-4">Home</div></a></li>
+                    <li><a href="#" class="text-white ps-3 pe-3">@livewire('leave-request-form')</a></li>
+                    <li><a href="#" class="text-white ps-3 pe-3">@livewire("reset-password-modal")</a></li>
+                    <li><a href="{{ route('employee.history') }}" class="text-white ps-3 mt-3 pe-3 ms-3"><div class="ps-4">Attendance History</div></a></li>   
+                    <li><a href="#" class="text-white ps-3 pe-3">@livewire('logout-button')</a></li>
+                </ul>
+            </div>
         </div>
+        <nav class="navbar" style="background-color:#003366">
+            <button class="navbar-toggler bg-white ms-4" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon " style="color:white"></span>
+            </button>
+            <a class="navbar-brand text-semi-bold text-white" href="#" style="font-size:27px">Attendify</a>
+        </nav>
     </div>
+<div class="container">
+    <h3 class="mb-3 mt-3" style="color:#003366">Attendance History</h3>
 
-    <div>
+    <div class="table-responsive">
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Leaves</th>
-                    <th>Late</th>
-                    <th>On Time</th>
-                    <th>Details</th>
+                    <th>Check In Time</th>
+                    <th>Check Out Time</th>
+                    <th>Status</th>
                 </tr>
             </thead>
             <tbody>
-                @forelse($employees as $employee)
+                @forelse ($attendances as $attendance)
                     <tr>
-                        <td>{{ $employee->name }}</td>
-                        <td>{{ $employee->email }}</td>
-                        <td>{{ $employee->leaves_count }}</td>
-                        <td>{{ $employee->late_count }}</td>
-                        <td>{{ $employee->on_time_count }}</td>
-                        <td>
-                            <a href="#" wire:click.prevent="showEmployeeHistory({{ $employee->id }})" style="color:#003366">
-                                View Attendance Details
-                            </a>
-                        </td>
+                        <td>{{ $attendance->check_in ? $attendance->check_in->format('Y-m-d h:i A') : 'N/A' }}</td>
+                        <td>{{ $attendance->check_out ? $attendance->check_out->format('Y-m-d h:i A') : 'Ongoing' }}</td>
+                        <td>{{ $attendance->status }}</td>
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="6" class="text-center">No employees found.</td>
+                        <td colspan="3" class="text-center">No attendance records found.</td>
                     </tr>
                 @endforelse
             </tbody>
+
         </table>
     </div>
 
-    {{ $employees->links() }}
-
-    <!-- Attendance Details Modal -->
-    <div x-data="{ show: @entangle('showAttendanceModal') }" x-show="show" class="modal fade show" style="display: block;" aria-modal="true" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">{{ $selectedEmployeeName }}'s Attendance History</h5>
-                    <button type="button" class="btn-close" aria-label="Close" wire:click="closeAttendanceModal"></button>
-                </div>
-                <div class="modal-body">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Date</th>
-                                <th>Check-In</th>
-                                <th>Check-Out</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse($attendanceHistory as $attendance)
-                                <tr>
-                                    <td>{{ $attendance->check_in->format('Y-m-d') }}</td>
-                                    <td>{{ $attendance->check_in->format('H:i:s') }}</td>
-                                    <td>{{ $attendance->check_out ? $attendance->check_out->format('H:i:s') : 'N/A' }}</td>
-                                    <td>{{ $attendance->status }}</td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="4" class="text-center">No attendance records found.</td>
-                                </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+    <!-- Pagination Controls -->
+    <div class="d-flex justify-content-center mt-3">
+        {{ $attendances->links() }}
     </div>
 </div>
-
-<script>
-    document.addEventListener('livewire:load', function () {
-        window.addEventListener('close-modal', event => {
-            var attendanceModal = new bootstrap.Modal(document.getElementById('attendanceModal'));
-            attendanceModal.hide();
-        });
-    });
-</script>
+</div>
--- resources/views/livewire/employee-list.blade.php
+++ resources/views/livewire/employee-list.blade.php
@@ -50,44 +50,43 @@
     {{ $employees->links() }}
 
     <!-- Attendance Details Modal -->
-<div class="modal fade @if($showAttendanceModal) show @endif" style="display: @if($showAttendanceModal) block @else none @endif;" tabindex="-1" role="dialog" aria-modal="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="addEmployeeModalLabel">Attendance Details for {{ $employee->name }}</h5>
-            <button type="button" class="btn btn-primary" wire:click="exportAttendance({{ $employee->id }})">Export to PDF</button>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-            <div class="modal-body">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Check-In</th>
-                            <th>Check-Out</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse($attendanceHistory as $attendance)
+    <div class="modal fade @if($showAttendanceModal) show @endif" style="display: @if($showAttendanceModal) block @else none @endif;" tabindex="-1" role="dialog" aria-modal="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">{{ $selectedEmployeeName }}'s Attendance History</h5>
+                    <button type="button" class="btn-close" aria-label="Close" wire:click="closeAttendanceModal"></button>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-striped">
+                        <thead>
                             <tr>
-                                <td>{{ $attendance->check_in->format('Y-m-d') }}</td>
-                                <td>{{ $attendance->check_in->format('H:i:s') }}</td>
-                                <td>{{ $attendance->check_out ? $attendance->check_out->format('H:i:s') : 'N/A' }}</td>
-                                <td>{{ $attendance->status }}</td>
+                                <th>Date</th>
+                                <th>Check-In</th>
+                                <th>Check-Out</th>
+                                <th>Status</th>
                             </tr>
-                        @empty
-                            <tr>
-                                <td colspan="4" class="text-center">No attendance records found.</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @forelse($attendanceHistory as $attendance)
+                                <tr>
+                                    <td>{{ $attendance->check_in->format('Y-m-d') }}</td>
+                                    <td>{{ $attendance->check_in->format('H:i:s') }}</td>
+                                    <td>{{ $attendance->check_out ? $attendance->check_out->format('H:i:s') : 'N/A' }}</td>
+                                    <td>{{ $attendance->status }}</td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="4" class="text-center">No attendance records found.</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
 
 <script>
     document.addEventListener('livewire:load', function () {
